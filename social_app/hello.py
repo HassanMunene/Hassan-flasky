f58f366a3472a6a52e20363f7d79dda5b81431af
HassanMunene <awanzihassan@gmail.com>
2023-09-01 15:49:21 +0300

    I have added email functionality where when a user registers the admin is informed that someone has joined the program

--- social_app/hello.py
+++ social_app/hello.py
@@ -1,40 +1,57 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'my sectret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://hassan:munene14347@localhost/flasky'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# This section below is configuring the application with different settings
+# such as sqlalchemy connections and email connections
+app.config['SECRET_KEY'] = 'my sectret boy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://hassan:munene14347@localhost/flasky'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'awanzihassan@gmail.com'
+app.config['MAIL_PASSWORD'] = 'xuhhwjkcsscxdmms'
+app.config['MAIL_SUBJECT'] = '[Flasky]'
+app.config['MAIL_SENDER'] = 'Flasky Admin <awanzihassan@gmail.com>'
+app.config['FLASKY_ADMIN'] = 'sultanhamud081@gmail.com'
+
+# This section below is to initialize the various extensions thats my application will use
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
+# The section we are defining database models that will be mapped into tables in mysql server
 class Role(db.Model):
     """
-    the model that will map into roles table
+    will be mapped to roles table
     """
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.Relationship('User', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         """
-        string representation of the object
+        string representation of roles
         """
-        return '<Role %r>' %self.name
+        return '<Role {}>'.format(self.name)
 
 class User(db.Model):
     """
-    The model that will map into the users table
+    will be mapped into users table
     """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -43,49 +60,55 @@ class User(db.Model):
 
     def __repr__(self):
         """
-        string representation of the object
+        string rep of the object
         """
-        return '<User %r>' %self.username
+        return '<User {}>'.format(self.username)
 
-"""
-This is the sections where we define the forms that we will be using now
-"""
+# This next section we will be defining the form classes that will be used for input
 class NameForm(FlaskForm):
     """
-    the form for inserting the name
+    will be rendered to be a form for user name input and submission button
     """
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField("What's your name?", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+# This section we will define a function called send_email that will be used to send emails
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['MAIL_SUBJECT'] + subject, sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template+'.txt', **kwargs)
+    msg.html = render_template(template+'.html', **kwargs)
+    mail.send(msg)
 
-"""
-In this sections we will use the shell context processor to automatically add some objects
-to our flask shell instead of manually importing them all the time
-"""
+# Here we are defining the objects that will be automatically available to our shell when we load the flask shell
 @app.shell_context_processor
 def make_shell_context():
-    """
-    return a dctionary of the objects you want to be
-    automatically loaded
-    """
     return {
-        'database': db,
+        'db': db,
         'User': User,
         'Role': Role,
+        'mail': Mail
     }
-"""
-Now here lets define the routes for our small application
-"""
+
+# In this next section we are defining the routes that our application will use
 @app.errorhandler(404)
 def page_not_found(e):
+    """
+    handle 404 error status code
+    """
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    """
+    handler error from the server
+    """
     return render_template('500.html'), 500
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    handle the root route of the app
+    """
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
@@ -94,9 +117,12 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+
         else:
             session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known'))
-
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
