dc010181a56bd625eed9d073b483fefbcf5fe525
HassanMunene <awanzihassan@gmail.com>
2023-09-17 11:14:42 +0300

    Added the avatar images from gravatar

--- social_app/app/models.py
+++ social_app/app/models.py
@@ -1,10 +1,11 @@
-from flask import current_app
+from flask import current_app, request
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from itsdangerous.serializer import Serializer
 from datetime import datetime
+import hashlib
 
 class Permission:
     FOLLOW = 1
@@ -100,16 +101,19 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    avatar_hash = db.Column(db.String(64))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
+
         if self.role is None:
             if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
-
     @property
     def password(self):
         """
@@ -182,6 +186,17 @@ class User(UserMixin, db.Model):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
         db.session.commit()
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        """
+        will generate some avator images for user profile using md5 hash
+        """
+        url = 'https://secure.gravatar.com/avatar'
+        hash = self.avatar_hash or self.gravatar_hash()
+        return f'{url}/{hash}?s={size}&d={default}&r={rating}'
 
 #---------------------------------------------------------------------------------------------------------------------------------------------
 @login_manager.user_loader
