4754e1559c70167c677fc008b9d8e11c5d0ace74
HassanMunene <awanzihassan@gmail.com>
2023-09-20 09:32:15 +0300

    We've added permanent links to each post in the app

--- social_app/app/main/views.py
+++ social_app/app/main/views.py
@@ -92,3 +92,13 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+@main.route('/post/<int:id>')
+def post(id):
+    """
+    This route will handle a specific url of a post.
+    Basically in our app each post will have a specific
+    unique url based on its id
+    """
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
