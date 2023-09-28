3d1d2d882a0bedae61e4440c239c564c8951dd8d
HassanMunene <awanzihassan@gmail.com>
2023-09-28 17:42:57 +0300

    I have intergrated the test coverage report with the unittest to give me an overview of the tests im making

--- social_app/flasky.py
+++ social_app/flasky.py
@@ -2,9 +2,17 @@ import os
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role, Permission
+import sys
+import click
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 @app.shell_context_processor
 def make_shell_context():
@@ -19,11 +27,31 @@ def make_shell_context():
         'Permission': Permission
     }
 
+
+# In the section below we will intergrate the coverage with our test runner so that when our test runs we
+# can determine which features of our app that have not yet been tested so that we implement test to them
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False, help='run test with code coverage')
+def test(coverage):
     """
-    run the unit tests
+    This part of the code utilizes the above decorator that is used to create custom commands
+    our application. In this case we are defining a command named test that will be used to
+    run the unit tests of the app.
     """
+
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     test = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(test)
+    if COV:
+        COV.stop()
+        COV.save()
+        print("Coverage summary:")
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print("HTML version: file://{}/index.html".format(covdir))
+        COV.erase()
