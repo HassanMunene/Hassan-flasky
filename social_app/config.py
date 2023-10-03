fcff460a154aa5d444626634dafeed4d4358801c
HassanMunene <awanzihassan@gmail.com>
2023-10-03 10:33:21 +0300

    added the configuration for the heroku deployment

--- social_app/config.py
+++ social_app/config.py
@@ -53,5 +53,6 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig
     'default': DevelopmentConfig
 }
